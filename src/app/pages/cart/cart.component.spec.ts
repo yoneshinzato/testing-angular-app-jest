b789ca763b1e66b72bcb6a965bc10fcff7031e01
Yone Shinzato <shinzato.yone@capco.com>
2023-01-30 12:20:12 -0300

    how to test a method with return

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -4,7 +4,32 @@ import { HttpClientTestingModule } from "@angular/common/http/testing"
 import { BookService } from "../../services/book.service";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/compiler";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Book } from '../../models/book.model';
 
+
+const listBook: Book[] = [
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 15,
+        amount: 2
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 20,
+        amount: 1
+    },
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 8,
+        amount: 7
+    },
+]
 
 describe('Cart component', () => {
 
@@ -37,6 +62,14 @@ describe('Cart component', () => {
 
     it('should create CartComponent', () => {
         expect(component).toBeTruthy();
+    })
+
+
+    it('getTotalPrice returns an amount', () => {
+       const totalPrice = component.getTotalPrice(listBook)
+       expect(totalPrice).toBeGreaterThan(0)
+    //    expect(totalPrice).not.toBe(0)
+    //    expect(totalPrice).not.toBeNull()
     })
 
 })
